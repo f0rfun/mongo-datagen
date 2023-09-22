9b4ae977979859857aa6905bc356d53305d67fc0
jianghua <jhwu87@gmail.com>
2023-09-23 00:03:20 +0800

    Create cable collection with data

--- main.go
+++ main.go
@@ -4,13 +4,32 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type OilPressureSensors struct {
+	SensorID int64
+	Section  uint8
+	Phase    string
+	Endpoint string
+}
+
+type Cables struct {
+	CircuitID          uint8
+	CircuitName        string
+	CircuitVoltage     string
+	Status             string
+	SourceSubstation   string
+	TargetSubstation   string
+	CircuitType        string
+	FeederNumber       uint8
+	OilPressureSensors []OilPressureSensors
+}
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
@@ -41,250 +60,65 @@ func main() {
 
 	// database and collection
 	database := mongoClient.Database("asset")
-	cableCollection := database.Collection("cable")
+	cableCollection := database.Collection("cables")
 	cableCollection.Drop(ctx)
 
-	cablePressureCollection := database.Collection("cablePressure")
-	cablePressureCollection.Drop(ctx)
+	phases := []string{"red", "yellow", "blue"}
+	endpoints := []string{"source", "destination"}
+	status := []string{"active", "inactive"}
 
-	// insert one cable
-	aCable := bson.M{
-		"gemsID":           "4002",
-		"circuitName":      "Ayer Rajah - Labrador F2",
-		"sourceSubstation": "Ayer Rajah 400kV",
-		"targetSubstation": "Labrador 400kV",
-		"status":           "active",
-		"voltageLevel":     "400",
-		"feederNumber":     "2",
-		"numberOfSection":  "5",
-		"circuitType":      "oil-filled",
-		"assetType":        "CABLE",
-		"createdAt":        time.Now(),
-		"createdBy":        "me",
-		"updatedAt":        time.Now(),
-		"updatedBy":        "me",
-	}
-	insertedCableResult, err := cableCollection.InsertOne(ctx, aCable)
+	// cablePressureCollection := database.Collection("cablePressure")
+	// cablePressureCollection.Drop(ctx)
 
-	if err != nil {
-		log.Fatalf("inserted error : %v", err)
-		return
-	}
-	fmt.Println("======= inserted cable id ================")
-	log.Printf("inserted ID is : %v", insertedCableResult.InsertedID)
+	// Generate and insert random data
+	for i := 1; i <= 65; i++ {
+		var oilPressureSensors []OilPressureSensors
+		// max number of pressure sensors per cable = max no. of Sections * number of readings per
+		// section * number of phases
+		// e.g. 8 * 2 * 3 = 48
+		for j := 1; j <= 48; j++ {
+			randomPhaseIndex := rand.Intn(len(phases))
+			randomEndpointIndex := rand.Intn(len(endpoints))
 
-	// setting historical threshold
-	thresholdOld := bson.M{
-		"high":      10.5,
-		"low":       0.05,
-		"startedAt": time.Now(),
-		"endedAt":   time.Now(),
-		"updatedAt": nil,
-	}
-	thresholdNew := bson.M{
-		"high":      5.5,
-		"low":       0.05,
-		"startedAt": time.Now(),
-		"endedAt":   time.Now(),
-		"updatedAt": nil,
-	}
-	threshold := bson.A{}
-	threshold = append(threshold, thresholdOld)
-	threshold = append(threshold, thresholdNew)
+			sensor := OilPressureSensors{
+				SensorID: rand.Int63n(48),
+				Section:  uint8(rand.Intn(8)),
+				Phase:    phases[randomPhaseIndex],
+				Endpoint: endpoints[randomEndpointIndex],
+			}
+			oilPressureSensors = append(oilPressureSensors, sensor)
+		}
 
-	// insert one cablePressure
-	aCablePressure := bson.M{
-		"gemsID":    nil,
-		"sensorID":  "98762",
-		"assetType": "CABLE_PRESSURE",
-		"threshold": threshold,
+		randomStatusIndex := rand.Intn(len(status))
 
-		"createdAt": time.Now(),
-		"createdBy": "me",
-		"updatedAt": time.Now(),
-		"updatedBy": "me",
-	}
-	insertedCablePressureResult, err := cablePressureCollection.InsertOne(ctx, aCablePressure)
+		cable := Cables{
+			CircuitID:          uint8(rand.Intn(65)),
+			CircuitName:        randomString(8),
+			CircuitVoltage:     randomString(3),
+			Status:             status[randomStatusIndex],
+			SourceSubstation:   randomString(8),
+			TargetSubstation:   randomString(8),
+			CircuitType:        "OIL-FILLED",
+			FeederNumber:       uint8(rand.Intn(5)),
+			OilPressureSensors: oilPressureSensors,
+		}
 
-	if err != nil {
-		log.Fatalf("inserted error : %v", err)
-		return
-	}
-	fmt.Println("======= inserted cable pressure id ================")
-	log.Printf("inserted ID is : %v", insertedCablePressureResult.InsertedID)
-
-	// query all cable data
-	fmt.Println("== query all cable data ==")
-	cursorCable, err := cableCollection.Find(ctx, options.Find())
-	if err != nil {
-		log.Fatalf("find collection err : %v", err)
-		return
-	}
-	var queryResult []bson.M
-	if err := cursorCable.All(ctx, &queryResult); err != nil {
-		log.Fatalf("query cable result")
-		return
+		_, err := cableCollection.InsertOne(ctx, cable)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	for _, doc := range queryResult {
-		fmt.Println(doc)
+	fmt.Println("Data inserted successfully.")
+}
+
+func randomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[r.Intn(len(charset))]
 	}
-
-	// query all cable pressure data
-	fmt.Println("== query all cable pressure data ==")
-	cursorPressure, err := cablePressureCollection.Find(ctx, options.Find())
-	if err != nil {
-		log.Fatalf("find collection err : %v", err)
-		return
-	}
-	var queryCablePressureResult []bson.M
-	if err := cursorPressure.All(ctx, &queryCablePressureResult); err != nil {
-		log.Fatalf("query cable pressure result")
-		return
-	}
-
-	for _, doc := range queryCablePressureResult {
-		fmt.Println(doc)
-	}
-
-	// // insert many data
-	// fmt.Println("=========== inserted many data ===============")
-	// insertedManyDocument := []interface{}{
-	// 	bson.M{
-	// 		"name":       "Andy",
-	// 		"content":    "new test content",
-	// 		"bank_money": 1500,
-	// 		"create_at":  time.Now().Add(36 * time.Hour),
-	// 	},
-	// 	bson.M{
-	// 		"name":       "Jack",
-	// 		"content":    "jack content",
-	// 		"bank_money": 800,
-	// 		"create_at":  time.Now().Add(12 * time.Hour),
-	// 	},
-	// }
-
-	// insertedManyResult, err := cableCollection.InsertMany(ctx, insertedManyDocument)
-	// if err != nil {
-	// 	log.Fatalf("inserted many error : %v", err)
-	// 	return
-	// }
-
-	// for _, doc := range insertedManyResult.InsertedIDs {
-	// 	fmt.Println(doc)
-	// }
-
-	// fmt.Println("=========== query specific data =====================")
-	// // query specific data
-	// filter := bson.D{
-	// 	bson.E{
-	// 		Key: "bank_money",
-	// 		Value: bson.D{
-	// 			bson.E{
-	// 				Key:   "$gt",
-	// 				Value: 900,
-	// 			},
-	// 		},
-	// 	},
-	// }
-
-	// filterCursor, err := cableCollection.Find(
-	// 	ctx,
-	// 	filter,
-	// )
-	// if err != nil {
-	// 	log.Fatalf("filter query data error : %v", err)
-	// 	return
-	// }
-	// var filterResult []bson.M
-	// err = filterCursor.All(ctx, &filterResult)
-	// if err != nil {
-	// 	log.Fatalf("filter result %v", err)
-	// 	return
-	// }
-
-	// for _, filterDoc := range filterResult {
-	// 	fmt.Println(filterDoc)
-	// }
-
-	// updateManyFilter := bson.D{
-	// 	bson.E{
-	// 		Key:   "name",
-	// 		Value: "michael",
-	// 	},
-	// }
-
-	// updateSet := bson.D{
-	// 	bson.E{
-	// 		Key: "$set",
-	// 		Value: bson.D{
-	// 			bson.E{
-	// 				Key:   "bank_money",
-	// 				Value: 2000,
-	// 			},
-	// 		},
-	// 	},
-	// }
-	// // update
-	// updateManyResult, err := cableCollection.UpdateMany(
-	// 	ctx,
-	// 	updateManyFilter,
-	// 	updateSet,
-	// )
-	// if err != nil {
-	// 	log.Fatalf("update error : %v", err)
-	// 	return
-	// }
-
-	// fmt.Println("========= updated modified count ===========")
-	// fmt.Println(updateManyResult.ModifiedCount)
-
-	// // check if updated with find solution
-	// checkedCursor, err := cableCollection.Find(
-	// 	ctx,
-	// 	bson.D{
-	// 		bson.E{
-	// 			Key:   "name",
-	// 			Value: "michael",
-	// 		},
-	// 	},
-	// )
-	// if err != nil {
-	// 	log.Fatalf("check result error : %v", err)
-	// 	return
-	// }
-	// var checkedResult []bson.M
-	// err = checkedCursor.All(ctx, &checkedResult)
-	// if err != nil {
-	// 	log.Fatalf("get check information error : %v", err)
-	// 	return
-	// }
-	// fmt.Println("=========== checked updated result ==============")
-	// for _, checkedDoc := range checkedResult {
-	// 	fmt.Println(checkedDoc)
-	// }
-	// fmt.Println("===============================")
-	// // delete Many
-
-	// deleteManyResult, err := cableCollection.DeleteMany(
-	// 	ctx,
-	// 	bson.D{
-	// 		bson.E{
-	// 			Key: "bank_money",
-	// 			Value: bson.D{
-	// 				bson.E{
-	// 					Key:   "$lt",
-	// 					Value: 1000,
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// )
-	// if err != nil {
-	// 	log.Fatalf("delete many data error : %v", err)
-	// 	return
-	// }
-	// fmt.Println("===== delete many data modified count =====")
-	// fmt.Println(deleteManyResult.DeletedCount)
-
+	return string(result)
 }
